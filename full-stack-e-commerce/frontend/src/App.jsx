1bf4c16b8f6d9ee879cc44509f0f4f6d8350cd45
Bobby Connolly <bobbyconnolly@bobbys-mbp.lan>
2023-01-19 19:40:04 -0500

    connect frontend to backend

--- full-stack-e-commerce/frontend/src/App.jsx
+++ full-stack-e-commerce/frontend/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import "./App.css"
 
 import { storeName, storeDescription } from "./assets/store-info.json"
-import ceramicMugsAndFlasks from "./assets/products.json"
+
+// moved to the server
+// import ceramicMugsAndFlasks from "./assets/products.json"
 
 export function App() {
     const [currentView, setCurrentView] = useState("product-list-view")
     const [selectedProductId, setSelectedProductId] = useState(null)
+    const [products, setProducts] = useState([])
+
+    // console.log("products", products)
+
+    useEffect(() => {
+        fetch("http://localhost:3000/products")
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                // console.log("data", data)
+                setProducts(data)
+            })
+    }, [])
 
     function productSelected(id) {
         console.log("ID", id)
@@ -25,14 +41,12 @@ export function App() {
     if (currentView === "product-list-view") {
         renderedView = (
             <ProductList
-                ceramicMugsAndFlasks={ceramicMugsAndFlasks}
+                products={products}
                 productSelected={(id) => productSelected(id)}
             />
         )
     } else if (currentView === "single-product-view") {
-        const foundProduct = ceramicMugsAndFlasks.find(
-            (el) => el.id === selectedProductId
-        )
+        const foundProduct = products.find((el) => el.id === selectedProductId)
 
         renderedView = (
             <ProductCard
@@ -52,7 +66,7 @@ export function App() {
 }
 
 function ProductList(props) {
-    const items = props.ceramicMugsAndFlasks.map((item) => {
+    const items = props.products.map((item) => {
         return (
             <ProductCard
                 id={item.id}
--- full-stack-e-commerce/frontend/src/main.jsx
+++ full-stack-e-commerce/frontend/src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client"
 import { App } from "./App"
 import "./index.css"
 
+// fetch("http://127.0.0.1:3000/test")
+//     .then((res) => {
+//         return res.text()
+//     })
+//     .then((result) => {
+//         console.log("result", result)
+//     })
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <App />
